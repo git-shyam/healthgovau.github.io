99f2946b80f01e6923d42088d05271a5b3f6dd21
Denwah <Denwar@outlook.com>
2016-11-14 10:30:32 +1100

    first cut with request content type

--- app/js/data.js
+++ app/js/data.js
@@ -1,3 +1,26 @@
+var HealthGovAu = {};
+
+HealthGovAu.RequestData = (function () {
+	var pub = {};
+
+	pub.selectAll = fucntion(){
+		jQuery.ajax({
+		    url: 'http://api.everlive.com/v1/kykky4omulqg6532/Request/',
+		    type: "GET",
+		    headers: {},
+		    success: function(data){
+		        console.log(JSON.stringify(data));
+		    },
+		    error: function(error){
+		        alert(JSON.stringify(error));
+		    }
+		});		
+	}
+
+
+	return pub;
+})();
+
 $(function(){
 	console.log("data loaded");
 });
