35e3e83168f34ae2839fd0b356a359cff80bfd4f
Denwah <Denwar@outlook.com>
2016-11-14 10:35:40 +1100

    with deferred

--- app/js/data.js
+++ app/js/data.js
@@ -4,19 +4,24 @@ HealthGovAu.RequestData = (function () {
 	var pub = {};
 
 	pub.selectAll = fucntion(){
+		var deferred = jQuery.Deferred();
+
 		jQuery.ajax({
 		    url: 'http://api.everlive.com/v1/kykky4omulqg6532/Request/',
 		    type: "GET",
 		    headers: {},
 		    success: function(data){
 		        console.log(JSON.stringify(data));
+    			deferred.resolve(data);		        
 		    },
 		    error: function(error){
-		        alert(JSON.stringify(error));
+		        console.log(JSON.stringify(error));
+				deferred.reject(JSON.stringify(error);				        
 		    }
-		});		
-	}
+		});
 
+		return deferred.promise();			
+	}
 
 	return pub;
 })();
